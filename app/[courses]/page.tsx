eb7a929669332bcd78c157cf8a4c9e80112b8caa
Faizan Tariq <faizantariq498@gmail.com>
2023-04-09 15:16:33 +0500

    restricted pages

--- app/[courses]/page.tsx
+++ app/[courses]/page.tsx
@@ -1,17 +1,30 @@
 "use client"
 import React from 'react'
 import Hero from '../CoursesComponents/Hero'
-import CourseDesc from '../CoursesComponents/CourseDesc'
+import CourseDesc from '../CoursesComponents/CourseDesc';
+import { usePathname } from 'next/navigation'
 
 
 const Courses = () =>
+
 {
+  const pid= usePathname();
+
+  if(pid === '/BCC'||pid === '/CNC'||pid === 'IOT'||pid === 'AI'){
     return (
       <section>
         <Hero />
         <CourseDesc/>
       </section>
     )
+  }else{
+    return (
+      <section>
+        <p>404 Not Found</p>
+      </section>
+    )
+  }
+    
 }
 
 export default Courses
