02fc3d4f8a9eed0c68161739edcd65004b63f6ec
Faizan Tariq <faizantariq498@gmail.com>
2023-05-01 22:41:17 +0500

    Contentful

--- app/Components/Hero.tsx
+++ app/Components/Hero.tsx
@@ -2,8 +2,35 @@ import Wrapper from "./Wrapper"
 import Image from "next/image"
 
 import HeroImage from '../../public/HeroPoster.png'
+// import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+interface herodatatype {
+    fields: {
+        title: string,
+        description: string,
+        heroImage: object
+      }
+}
 
-const Hero = () => {
+const getHeroData = async ()=>{
+    try{
+        const heroSecData = await fetch('https://cdn.contentful.com/spaces/x6eewvg9w6p3/environments/master/entries/6AvnYkoyEjYLar6YRtSW2j?access_token=5RealmlGsXBDk80YvoPbdLbnYBf6166TIHtOwfTIdWo');
+
+        if(!heroSecData.ok){
+            console.log("Failed to Load data")
+            throw new Error("Failed to Load Data");
+        }
+
+        return heroSecData.json();
+
+    }catch(err){
+        console.log(err);
+    }
+
+}
+
+const Hero = async () => {
+    const heroData:herodatatype = await getHeroData();
+    console.log(heroData)
   return (
 
     <section>
@@ -13,14 +40,12 @@ const Hero = () => {
                  <div className=" basis-1/2 px-2">
                     <h6 className="text-green-700 underline font-bold">President Initiative of Artificial Intelligence and Computing</h6>
                     <h1 className="font-bold text-5xl py-3">
-                        Certified Web 3.0 and Metaverse Developer
+                        {heroData.fields.title}
                     </h1>
                     <p className="text-justify text-gray-500">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum quod sint ipsam amet id nam ducimus ab.
+                       {heroData.fields.description}
                     </p>
-                    <p className="text-justify py-3 text-gray-500">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi alias iste obcaecati architecto consequatur repellat, tempora asperiores impedit nemo porro eum aperiam official.
-                    </p>
+                    
                     <button className="py-3 px-6 mt-4 text-center text-white rounded-full bg-blue-900 font-bold">
                         Learn More
                     </button>
